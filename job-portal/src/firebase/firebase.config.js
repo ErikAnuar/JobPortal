38e01a753b47002973c9e3417e35e7d40953ef4d
HoleHorse <tamirlankulzhanov@gmail.com>
2023-11-21 22:03:37 +0600

    setup

--- job-portal/src/firebase/firebase.config.js
+++ job-portal/src/firebase/firebase.config.js
@@ -1,19 +1,23 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_APIKEY,
-  authDomain: import.meta.env.VITE_AUTHDOMAIN,
-  projectId: import.meta.env.VITE_PROJECTID,
-  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
-  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
-  appId: import.meta.env.VITE_APPID
+  apiKey: "AIzaSyD50slTq6u7KFqEnGRTSd1i2h9cdnTvQJM",
+  authDomain: "jobportal-rmt.firebaseapp.com",
+  projectId: "jobportal-rmt",
+  storageBucket: "jobportal-rmt.appspot.com",
+  messagingSenderId: "494974081632",
+  appId: "1:494974081632:web:a3656d2c307bd06c35c105",
+  measurementId: "G-6LN7XSZF9W",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 export default app;
