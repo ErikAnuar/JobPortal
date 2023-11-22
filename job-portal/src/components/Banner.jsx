6db3fb6d6b2dcd4966a0da9525d33402d80ce094
HoleHorse <tamirlankulzhanov@gmail.com>
2023-11-22 19:49:56 +0600

    rm location bar

--- job-portal/src/components/Banner.jsx
+++ job-portal/src/components/Banner.jsx
@@ -27,19 +27,6 @@ const Banner = ({ handleInputChange, query }) => {
             <FiSearch className="absolute mt-2.5 ml-2 text-gray-400" />
           </div>
 
-          <div className="flex md:rounded-none rounded ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 md:w-1/3">
-            <input
-              type="text"
-              name="username"
-              id="username"
-              className="block flex-1 border-0 bg-transparent py-1.5 pl-8 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
-              placeholder="Location"
-              onChange={handleInputChange}
-              value={""}
-            />
-            <FiMapPin className="absolute mt-2.5 ml-2 text-gray-400" />
-          </div>
-
           <button
             type="submit"
             className="bg-blue py-2 px-8 text-white md:rounded-e-md md:rounded-s-none rounded"
